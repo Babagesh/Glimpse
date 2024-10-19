3ac17bcd41bef839e7965f6dec070ac156180af8
Jayden Moore <jaydenmoore456@gmail.com>
2024-10-19 05:55:25 -0700

    modified:   package-lock.json 	modified:   src/components/Navbar.js 	new file:   src/database.js

---
+++ src/database.js
@@ -0,0 +1,24 @@
+import firebase from "firebase"
+import "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRv2sUSBbgsnoJsT1LnUcsE6eFaXXzlDk",
+    authDomain: "glimpses-8bf56.firebaseapp.com",
+    projectId: "glimpses-8bf56",
+    storageBucket: "glimpses-8bf56.appspot.com",
+    messagingSenderId: "90716597482",
+    appId: "1:90716597482:web:94de9cb882f480504e7b93",
+    measurementId: "G-Q00N0G3WRX"
+};
+
+/*if (!firebase.apps.length) {
+    const app = firebase.initializeApp(firebaseConfig);
+}else {
+    firebase.app();
+}*/
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+export const auth = getAuth(app);
+export default app;
