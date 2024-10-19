a0f49acce81c1b7478e139cf396c35d157f3884c
Daniyl Nguyen <daniylnguyen776@gmail.com>
2024-10-19 09:59:40 -0700

    title cards for main screen

--- src/pages/glimpses.js
+++ src/pages/glimpses.js
@@ -1,25 +1,68 @@
 import React from 'react';
-import '../taskbar.css';
+import '../App.css';
 import { useNavigate } from 'react-router-dom' 
 
-export default function Taskbar() {
-  const navigate = useNavigate()
-
-  const onButtonClick = () => {
-    navigate("/glimpses")
-  }
-
+export default function glimpses() {
   return (
-    <div className="taskbar">
-      <div className="taskbar-option">
-        <button onClick={() => navigate("/existing")}>Contribute To Glimpse</button>
-        </div>
-      <div className="taskbar-option">
-        <button onClick={() => navigate("/new")}>New Glimpse</button>
-        </div>
-      <div className="taskbar-option">
-        <button onClick={() => navigate("/future")}>Future Glimpse</button>
-        </div>
+    <div>
+      <FileList />
+      <Taskbar />
     </div>
   );
 }
+
+  const Taskbar = () => {
+    const navigate = useNavigate()
+
+    return (
+      <div className="taskbar">
+        <div className="taskbar-option">
+          <button onClick={() => navigate("/existing")}>Contribute To Glimpse</button>
+          </div>
+        <div className="taskbar-option">
+          <button onClick={() => navigate("/new")}>New Glimpse</button>
+          </div>
+        <div className="taskbar-option">
+          <button onClick={() => navigate("/future")}>Future Glimpse</button>
+          </div>
+      </div>
+    );
+  }
+
+const files = [
+  {
+    title: 'Chicago',
+    thumbnail: '../public/Globe.png',
+  },
+  {
+    title: 'San Francisco',
+    thumbnail: '../public/Globe.png',
+  },
+  {
+    title: 'New York City',
+    thumbnail: '../public/Globe.png',
+  },
+];
+
+const FileList = () => {
+  return (
+    <div className="file-list">
+      {files.map((file, index) => (
+        <FileCard
+          key={index}
+          title={file.title}
+          thumbnail={file.thumbnail}
+        />
+      ))}
+    </div>
+  );
+};
+
+const FileCard = ({ title, thumbnail }) => {
+  return (
+    <div className="file-card">
+      <img src={thumbnail} alt={`${title} thumbnail`} />
+      <h3>{title}</h3>
+    </div>
+  );
+};
