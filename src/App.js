ab2f93edc81019c94e6e291b79a29661362f967f
Jayden Moore <jaydenmoore456@gmail.com>
2024-10-18 23:39:54 -0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/index.js 	new file:   src/pages/login.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import './App.css';
+import React, { useState } from 'react'
+import { redirect } from 'react-router-dom' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [emailError, setEmailError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
+
+    const onButtonClick = () => {
+      redirect("/login")
+    }
+
+    return (
+        <div>
+            <div>
+                Login
+            </div>
+            <br/>
+            <div>
+                <input
+                    value = {email}
+                    placeholder = "Email"
+                    onChange = { (event) => setEmail(event.target.value)}
+                    className = {'inputBox'}
+                />
+                <label className = "errorLabel">{emailError}</label>
+            </div>
+            <br/>
+            <div>
+                <input
+                    value = {password}
+                    placeholder = "Password"
+                    onChange = { (event) => setPassword(event.target.value)}
+                    className = {'inputBox'}
+                />
+                <label className = "errorLabel">{passwordError}</label>
+            </div>
+            <br/>
+            <div>
+                <input className = {'inputButton'} type = "button" onClick = {onButtonClick} value = {"Log in"}/>
+            </div>
+        </div>
+    );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import App from './App';
+import Login from "./pages/login"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/test",
+    element: <Login />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
