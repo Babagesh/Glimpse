312fdf564a0031a1a8e5e5ac3364a165d4968736
Jayden Moore <jaydenmoore456@gmail.com>
2024-10-19 05:10:25 -0700

    modified:   src/components/Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="z-50 px-8 py-6 bg-gradient-to-b from-black to-slate-900 shadow-lg text-white">
+    <div className="z-50 px-8 py-6 bg-gradient-to-b from-black to-slate-950 shadow-lg text-white">
       <div className="flex flex-row justify-center align-center mt-12">
           <div className="flex flex-col items-center mr-12">
             <text className="text-xl mb-4">Placeholder Title</text>
