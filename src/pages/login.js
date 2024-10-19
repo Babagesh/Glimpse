b763edfd66d21648b078ac50928b3d1f18e2691f
Jayden Moore <jaydenmoore456@gmail.com>
2024-10-19 09:59:03 -0700

    modified:   src/pages/login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -56,45 +56,6 @@ export default function Login() {
         </div>
         <Link to="/signup" className="mt-4 text-sky-600 text-md">Sign Up</Link>
       </div>
-      {/*
-        <div className="font-[sans-serif]">
-            <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
-                <div className="border border-gray-300 rounded-lg px-24 py-12 shadow-xl m-auto">
-                    <div className="text-gray-800 text-center text-3xl font-extrabold text-large bg-gradient-to-r from-[#cda882] to-[#eadbcb] inline-block text-transparent bg-clip-text">
-                        Glimpses
-                    </div>
-                    <br />
-                    <div className="bg-slate-950 size-xxl w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none">
-                        <input
-                            size={30}
-                            classname=""
-                            required
-                            type="text"
-                            value={email}
-                            placeholder="Email"
-                            onChange={(event) => setEmail(event.target.value)}
-                        />
-                        <label className="errorLabel">{emailError}</label>
-                    </div>
-                    <br />
-                    <div className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none">
-                        <input
-                            value={password}
-                            required
-                            placeholder="Password"
-                            onChange={(event) => setPassword(event.target.value)}
-                            className={'inputBox'}
-                        />
-                        <label className="errorLabel">{passwordError}</label>
-                    </div>
-                    <br />
-                    <div className="mt-8">
-                        <input type="button" className="w-full shadow-xl py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-gray-600 hover:bg-gray-700 focus:outline-none bg-gradient-to-r from-[#cda882] to-[#eadbcb]" onClick={onSubmit} value={'Log in'} />
-                    </div>
-                </div>
-            </div>
-        </div>
-        */}
     </div>
   );
 }
