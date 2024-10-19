236d089103c14d42c393d27823f0420950ab8135
Daniyl Nguyen <daniylnguyen776@gmail.com>
2024-10-19 07:22:34 -0700

    updated login page with style

--- src/App.js
+++ src/App.js
@@ -29,22 +29,26 @@ export default function Login() {
     }
 
     return (
-        <div>
-            <div>
-                Login
+        <div class="font-[sans-serif]">
+        <div class="min-h-screen flex fle-col items-center justify-center py-6 px-4">
+        <div class="border border-gray-300 rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-md:mx-auto">
+        <form class="space-y-4">
+            <div class="text-gray-800 text-center text-3xl font-extrabold text-large">
+                Glimpses
             </div>
             <br/>
-            <div>
+            <div class="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none">
                 <input
                     value = {email}
                     placeholder = "Email"
                     onChange = { (event) => setEmail(event.target.value)}
                     className = {'inputBox'}
+                    class = "size-xxl"
                 />
                 <label className = "errorLabel">{emailError}</label>
             </div>
             <br/>
-            <div>
+            <div class="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none">
                 <input
                     value = {password}
                     placeholder = "Password"
@@ -54,9 +58,12 @@ export default function Login() {
                 <label className = "errorLabel">{passwordError}</label>
             </div>
             <br/>
-            <div>
-              <input className={'inputButton'} type="button" onClick={onSubmit} value={'Log in'} />
+            <div class="!mt-8">
+              <input className={'inputButton'} type="button" class="w-full shadow-xl py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-gray-600 hover:bg-gray-700 focus:outline-none" onClick={onSubmit} value={'Log in'} />
             </div>
+        </form>
+        </div>
+        </div>
         </div>
     );
 }
